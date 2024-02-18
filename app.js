674a69a07cbb4ad3a03bd617a6e8da18a6567645
Vivek Sachan <viveksachan06@gmail.com>
2024-02-18 13:20:22 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// // Import required modules
+// const http = require('http');
+
+// // Create a server
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello, World!');
+// });
+
+// // Start the server
+// const port = 3000;
+// server.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}/`);
+// });
+
+console.log('Hello, World!')
